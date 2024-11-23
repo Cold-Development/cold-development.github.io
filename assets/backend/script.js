d7fbfb17e2e47e0977b3a4af6802ea2658ab963f
padrewin <97427230+padrewin@users.noreply.github.com>
2024-11-23 19:49:19 +0200

    update

--- assets/backend/script.js
+++ assets/backend/script.js
@@ -3,3 +3,10 @@ console.log(
     "background-color: #635aa7; color: white; border-radius: 8px;",
     "font-style: italic; color: white"
 );
+
+document.addEventListener('touchmove', (e) => {
+    if (e.target.closest('.scrollable')) {
+        return; // Permite scroll pe elementele cu clasa `scrollable`
+    }
+    e.preventDefault(); // Blochează scroll în alte părți
+});
